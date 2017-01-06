a6ffa0f1ad4563c96850589fff675b16a65e3736
PeterWarnickHanson <PHanson@legatus.solutions>
2017-01-06 12:32:26 -0700

    Initial commit

---
+++ src/main/java/solutions/legatus/MFeesApplication.java
@@ -0,0 +1,12 @@
+package solutions.legatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MFeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MFeesApplication.class, args);
+	}
+}
